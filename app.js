674c174626dc883cc90a70baa0985451a0a01f57
平野真一 <hiranoshinichi@hiranoshinichi-no-iMac.local>
2022-10-21 22:26:12 +0900

    完了未完了の実装が完了しました

---
+++ app.js
@@ -0,0 +1,17 @@
+$("#addTodo").click(function () {
+    const inputTodo = $("input").val();
+    $("#todoList").append("<li><input type='checkbox'> " + inputTodo + "</li>");
+
+    $("input").val("");
+    // inputを空にする
+});
+
+
+$(document).on("change", "input[type = checkbox]",function(){
+    if($(this).is(":checked")) {
+        $(this).parent().css("text-decoration", "line-through");
+    }else {
+        $(this).parent().css("text-decoration", "none");
+
+    }
+});
